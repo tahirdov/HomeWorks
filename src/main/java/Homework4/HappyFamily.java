97bc4138b1446f1b8d02f6685fdf49c2c30b5959
countmerloin <tahirdovtalabi@gmail.com>
2019-10-25 07:49:52 +0400

    - HW5 edited

--- src/main/java/Homework4/HappyFamily.java
+++ src/main/java/Homework4/HappyFamily.java
@@ -1,62 +1,64 @@
-//package Homework4;
-//
-//public class HappyFamily {
-//    public static void main(String[] args) {
-//
-//        System.out.println( "Family Grace:" );
-//        String[][] schedule1 = {
-//                {"Monday"}, {"Visit library"},
-//                {"Wednesday"}, {"Visit father's parents"},
-//                {"Friday"}, {"Visit mother's parents"}
-//        };
-//
-//        Pet pet1 = new Pet( "dog", "Gerbert", 3, 89,
-//                new String[]{"playing", "barking at cars", "sleeping"} );
-//
-//        Human grandpa1 = new Human( "Alexander", "Grace", 1942 );
-//        Human grandma1 = new Human( "Selene", "Grace", 1944 );
-//
-//        Human grandpa2 = new Human( "Geralt", "Hardone", 1943 );
-//        Human grandma2 = new Human( "Lily", "Hardone", 1948 );
-//
-//        Human father1 = new Human( "Mark", "Grace", 1972, 95, grandpa1, grandma1 );
-//        Human mother1 = new Human( "Victoria", "Grace", 1978, 98, grandpa2, grandma2 );
-//
-//        Human child1 = new Human( "Gerrard", "Grace", 1998, 92,
-//                father1, mother1, schedule1, pet1 );
-//
-//        child1.greetPet();
-//        child1.pet.respond();
-//        child1.pet.eat();
-//
-//        System.out.println();
-//        System.out.println( "Family D'Atre:" );
-//
-//        Pet pet2 = new Pet( "owl", "Hedwig", 4, 94,
-//                new String[]{"Screeching", "Playing with Grenda", "Sitting on Merloin's shoulder"} );
-//        Pet pet3 = new Pet( "cat", "Grenda", 2, 91,
-//                new String[]{"Eating", "Sleeping half of day", "Playing with Hedwig", "Scratching couch"} );
-//
-//        Human grandpa3 = new Human( "Estel", "D'Atre", 1959 );
-//        Human grandma3 = new Human( "Elizabeth", "D'Atre", 1966 );
-//
-//        Human grandpa4 = new Human( "Mark", "Oldhouse", 1960 );
-//        Human grandma4 = new Human( "Melody", "Oldhouse", 1962 );
-//
-//        Human father2 = new Human( "Merloin", "D'atre", 1986, 100, grandpa3, grandma3 );
-//        Human mother2 = new Human( "Esmeralda", "D'Atre", 1988, 88, grandpa4, grandma4 );
-//
-//        Human child2 = new Human( "Elenna", "D'Atre", 2014, 76, father2, mother2 );
-//        Human child3 = new Human( "Martin", "D'Atre", 2017, 62, father2, mother2 );
-//
-//        father2.pet = pet2;
-//        mother2.pet = pet3;
-//
-//        child2.pet = child3.pet = pet3;
-//        mother2.greetPet();
-//        mother2.pet.respond();
-//        child1.describePet();
-//
-//    }
-//}
-//
+package Homework4;
+
+public class HappyFamily {
+    public static void main(String[] args) {
+
+        System.out.println( "Family Grace:" );
+        String[][] schedule1 = {
+                {"Monday"}, {"Visit library"},
+                {"Wednesday"}, {"Visit father's parents"},
+                {"Friday"}, {"Visit mother's parents"}
+        };
+
+        Pet pet1 = new Pet( "dog", "Gerbert", 3, 89,
+                new String[]{"playing", "barking at cars", "sleeping"} );
+
+        Human grandpa1 = new Human( "Alexander", "Grace", 1942 );
+        Human grandma1 = new Human( "Selene", "Grace", 1944 );
+
+        Human grandpa2 = new Human( "Geralt", "Hardone", 1943 );
+        Human grandma2 = new Human( "Lily", "Hardone", 1948 );
+
+        Human father1 = new Human( "Mark", "Grace", 1972, 95, grandpa1, grandma1 );
+        Human mother1 = new Human( "Victoria", "Grace", 1978, 98, grandpa2, grandma2 );
+
+        Human child1 = new Human( "Gerrard", "Grace", 1998, 92,
+                father1, mother1, schedule1, pet1 );
+
+        child1.greetPet();
+        child1.getPet().respond();
+        child1.getPet().eat();
+
+        System.out.println();
+        System.out.println( "Family D'Atre:" );
+
+        Pet pet2 = new Pet( "owl", "Hedwig", 4, 94,
+                new String[]{"Screeching", "Playing with Grenda", "Sitting on Merloin's shoulder"} );
+        Pet pet3 = new Pet( "cat", "Grenda", 2, 91,
+                new String[]{"Eating", "Sleeping half of day", "Playing with Hedwig", "Scratching couch"} );
+
+        Human grandpa3 = new Human( "Estel", "D'Atre", 1959 );
+        Human grandma3 = new Human( "Elizabeth", "D'Atre", 1966 );
+
+        Human grandpa4 = new Human( "Mark", "Oldhouse", 1960 );
+        Human grandma4 = new Human( "Melody", "Oldhouse", 1962 );
+
+        Human father2 = new Human( "Merloin", "D'atre", 1986, 100, grandpa3, grandma3 );
+        Human mother2 = new Human( "Esmeralda", "D'Atre", 1988, 88, grandpa4, grandma4 );
+
+        Human child2 = new Human( "Elenna", "D'Atre", 2014, 76, father2, mother2 );
+        Human child3 = new Human( "Martin", "D'Atre", 2017, 62, father2, mother2 );
+
+        father2.setPet( pet2 );
+        mother2.setPet( pet3 );
+
+        child2.setPet( pet2 );
+        child3.setPet( pet3 );
+
+        mother2.greetPet();
+        mother2.getPet().respond();
+        child1.describePet();
+
+    }
+}
+
--- src/main/java/Homework4/Human.java
+++ src/main/java/Homework4/Human.java
@@ -3,33 +3,17 @@ package Homework4;
 import java.util.Arrays;
 
 public class Human {
-
-    Pet pet;
     private String name;
     private String surname;
     private int year;
     private int iq;
-    private String father;
-    private String mother;
+    private Human father;
+    private Human mother;
     private String[][] schedule;
+    private Pet pet;
 
-    Human(String name, String surname, int year) {
-        this.name = name;
-        this.surname = surname;
-        this.year = year;
-    }
-
-    Human(String name, String surname, int year, int iq, String father, String mother) {
-        this.name = name;
-        this.surname = surname;
-        this.year = year;
-        this.iq = iq;
-        this.father = father;
-        this.mother = mother;
-    }
-
-    Human(String name, String surname, int year, int iq, String father,
-          String mother, String[][] schedule, Pet pet) {
+    Human(String name, String surname, int year, int iq, Human father,
+          Human mother, String[][] schedule, Pet pet) {
         this.name = name;
         this.surname = surname;
         this.year = year;
@@ -40,10 +24,24 @@ public class Human {
         this.pet = pet;
     }
 
+    Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
     public Human() {
 
     }
 
+    Human(String name, String surname, int year, int iq, Human father, Human mother) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.father = father;
+        this.mother = mother;
+    }
 
     void greetPet() {
         System.out.println( "Hello " + pet.getNickname() + "!" );
@@ -60,5 +58,69 @@ public class Human {
     public String toString() {
         return String.format( "Human{name = %s, surname=%s, year = %d, iq = %d, schedule = %s",
                 name, surname, year, iq, Arrays.toString( schedule ) );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getDateOfYear() {
+        return year;
+    }
+
+    public void setDateOfYear(int year) {
+        this.year = year;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public String[][] getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String[][] schedule) {
+        this.schedule = schedule;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 }
