5e265fac55fc2011047088695c8f9b693dabff6a
countmerloin <tahirdovtalabi@gmail.com>
2019-10-27 15:39:18 +0300

    - HW6 test class added

---
+++ src/test/java/Homework6/FamilyTest.java
@@ -0,0 +1,60 @@
+package Homework6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FamilyTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void deleteChild() {
+    }
+
+    @Test
+    public void getMother() {
+    }
+
+    @Test
+    public void setMother() {
+    }
+
+    @Test
+    public void setFather() {
+    }
+
+    @Test
+    public void getChildren() {
+    }
+
+    @Test
+    public void setChildren() {
+    }
+
+    @Test
+    public void getPet() {
+    }
+
+    @Test
+    public void setPet() {
+    }
+
+    @Test
+    public void getFamily() {
+    }
+
+    @Test
+    public void setFamily() {
+    }
+
+    @Test
+    public void getChild() {
+    }
+
+    @Test
+    public void setChild() {
+    }
+}
